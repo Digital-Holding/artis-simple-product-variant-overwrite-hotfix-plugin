4d801c05eaadd71b601b592fede09d8e5deaeef3
IDCT Bartosz Pachołek <bartosz@idct.pl>
2020-10-13 13:22:41 +0200

    Usage of value resolver

--- src/Form/ProductTypeExtension.php
+++ src/Form/ProductTypeExtension.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace DH\ArtisSimpleProductVariantOverwriteHotfixPlugin\Form;
 
+use App\Entity\Product\Product;
+use App\Factory\Product\DHVariantSpecificationItemValueViewFactoryInterface;
+use DH\Artis\Product\Specification\SpecificationItem\SpecificationItemValueResolverInterface;
 use DH\ArtisSimpleProductVariantOverwriteHotfixPlugin\Subscriber\SimpleProductVariantHotfixEventSubscriber;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductVariantType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class ProductTypeExtension extends AbstractTypeExtension
 {
+    /** @var SpecificationItemValueResolverInterface */
+    protected $specificationItemValueResolver;
+
+    public function __construct(SpecificationItemValueResolverInterface $specificationItemValueResolver)
+    {
+        $this->specificationItemValueResolver = $specificationItemValueResolver;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->addEventSubscriber(new SimpleProductVariantHotfixEventSubscriber());
+            ->addEventSubscriber(new SimpleProductVariantHotfixEventSubscriber($this->specificationItemValueResolver));
     }
 
     public static function getExtendedTypes(): iterable
--- src/Subscriber/SimpleProductVariantHotfixEventSubscriber.php
+++ src/Subscriber/SimpleProductVariantHotfixEventSubscriber.php
@@ -2,12 +2,28 @@
 
 namespace DH\ArtisSimpleProductVariantOverwriteHotfixPlugin\Subscriber;
 
+use App\Entity\Product\ProductVariantInterface;
+use App\Entity\Product\ProductVariantSpecificationItemInterface;
+use App\Entity\Product\ProductVariantSpecificationItemValue;
+use App\Entity\Product\ProductVariantSpecificationItemValueInterface;
+use App\Entity\Product\ProductVariantSpecificationItemValues;
+use App\Entity\Product\ProductVariantSpecificationItemValuesInterface;
+use App\Factory\Product\DHVariantSpecificationItemValueViewFactoryInterface;
+use DH\Artis\Product\Specification\SpecificationItem\SpecificationItemValueResolverInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
 class SimpleProductVariantHotfixEventSubscriber implements EventSubscriberInterface
 {
+    /** @var SpecificationItemValueResolverInterface */
+    protected $specificationItemValueResolver;
+
+    public function __construct(SpecificationItemValueResolverInterface $specificationItemValueResolver)
+    {
+        $this->specificationItemValueResolver = $specificationItemValueResolver;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -24,6 +40,31 @@ class SimpleProductVariantHotfixEventSubscriber implements EventSubscriberInterf
         foreach ($variantForm->all() as $fieldName => $val) {
             if (!isset($variantData[$fieldName]) && $fieldName !== 'specificationItemValues') {
                 $variantForm->remove($fieldName);
+            }
+        }
+
+        //fix for specification item values
+        /** @var ProductVariantInterface */
+        $variant = $variantForm->getNormData();
+
+        if (!isset($data['variant']['specificationItemValues'])) {
+            $data['variant']['specificationItemValues'] = [];
+        }
+
+
+        /** @var ProductVariantSpecificationItemValuesInterface */
+        foreach ($variant->getSpecificationItemValues() as $item)
+        {
+            $code = $item->getSpecificationItemValueCode();
+            $type = $item->getSpecificationItemValue()->getItem()->getType();
+            $val = $this->specificationItemValueResolver->getSpecificationItemValueByType($type, $item, true);
+            $field = $this->specificationItemValueResolver->getSpecificationItemValueFieldByType($type, $item);
+
+            //special case for booleans
+            if (!isset($data['variant']['specificationItemValues'][$code]) && $val !== true) {
+                $data['variant']['specificationItemValues'][$code] = [
+                    $field => $val
+                ];
             }
         }
 
